237ead2e4fac023502ccde2349c773d888761446
Jason Wong <wongj003@gmail.com>
2024-10-25 18:19:28 -0400

    added vercel and cors code for deployment

--- server/app.js
+++ server/app.js
@@ -10,7 +10,25 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors());
+const prodOrigins = [process.env.ORIGIN_1, process.env.ORIGIN_2];
+const devOrigin = ['http://localhost:5173'];
+const allowedOrigins =
+  process.env.NODE_ENV === 'production' ? prodOrigins : devOrigin;
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin)) {
+        console.log(origin, allowedOrgins);
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  })
+);
 app.use(express.json());
 
 //Routes
